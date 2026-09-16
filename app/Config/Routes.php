8939caac89c740e3ee4aeafd2b00be449269493b
donifebrian84 <donifebrian84@gmail.com>
2026-09-16 19:11:49 +0700

    refactor: standardize all master data routes using RESTful resource controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,14 +5,9 @@ use CodeIgniter\Router\RouteCollection;
 /** @var RouteCollection $routes */
 $routes->get('/', 'Home::index');
 
-$routes->get('akun1', 'Akun1::index');
-$routes->get('akun1/new', 'Akun1::new');
-$routes->post('akun1', 'Akun1::create');
-$routes->get('akun1/edit/(:segment)', 'Akun1::edit/$1');
-$routes->put('akun1/update/(:segment)', 'Akun1::update/$1');
-$routes->delete('akun1/delete/(:segment)', 'Akun1::delete/$1');
+//$routes->resource() pada app/Config/Routes.php agar secara otomatis mendaftarkan 7 rute RESTful (index, new, create, edit, update, delete, show): RESTful Resource Controller
+$routes->resource('akun1', ['controller' => 'Akun1']);
 
-//$routes->resource() pada app/Config/Routes.php agar secara otomatis mendaftarkan 7 rute RESTful (index, new, create, edit, update, delete, show): RESTful Resource Controller untuk Akun 2
 $routes->resource('akun2', ['controller' => 'Akun2']);
 
 $routes->resource('akun3', ['controller' => 'Akun3']);
